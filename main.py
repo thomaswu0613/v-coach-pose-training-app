87776ba02e04b5bbb8b57d2e9228eab2e2b12e31
Timmy Wu <wutimmy01@gmail.com>
2021-09-22 04:56:10 +0800

    added origin code

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import cv2
+import yaml
 import mediapipe as mp
-import numpy as np
 import traceback
+import numpy as np
+import os
+mp_drawing = mp.solutions.drawing_utils
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-count=1
-
-cap = cv2.VideoCapture(2)
-
-#cap = cv2.VideoCapture("test/pose.pose.jpg")
-
+camera_index = 0
+same = False
+overall = []
 skip = False
+stage = 1
+exceise_name = "test_exceise"
 
 
+
+#calc angle
 def calculate_angle(a,b,c):
     a = np.array(a) # First
     b = np.array(b) # Mid
@@ -28,114 +32,158 @@ def calculate_angle(a,b,c):
         angle = 360-angle
         
     return angle 
-"""
-def get_body_angle_by_name(landmarks,angle_name):
-    body_angle_points = {
-                    "armpit_left":[mp_pose.PoseLandmark.LEFT_ELBOW.value,mp_pose.PoseLandmark.LEFT_SHOULDER.value,mp_pose.PoseLandmark.LEFT_WRIST.value],
-                    "armpit_right":[mp_pose.PoseLandmark.RIGHT_ELBOW.value,mp_pose.PoseLandmark.RIGHT_SHOULDER.value,mp_pose.PoseLandmark.RIGHT_WRIST.value],
-                    "elbow_left":[mp_pose.PoseLandmark.LEFT_SHOULDER.value,mp_pose.PoseLandmark.LEFT_ELBOW.value,mp_pose.PoseLandmark.LEFT_WRIST.value],
-                    "elbow_right":[mp_pose.PoseLandmark.RIGHT_SHOULDER.value,mp_pose.PoseLandmark.RIGHT_ELBOW.value,mp_pose.PoseLandmark.RIGHT_WRIST.value],
-                    "knee_left_inner":[mp_pose.PoseLandmark.LEFT_HIP.value,mp_pose.PoseLandmark.LFET_KNEE.value,mp_pose.PoseLandmark.LEFT_ANKLE.value],
-                    "knee_right_inner":[mp_pose.PoseLandmark.RIGHT_HIP.value,mp_pose.PoseLandmark.RIGHT_KNEE.value,mp_pose.PoseLandmark.RIGHT_ANKLE.value]
-                    }
-    if angle_name not in body_angle_points.keys():
-        return None
-    tmp = []
-    tmp = body_angle_points[str(angle_name)]
-    return calculate_angle([landmarks[tmp[0]].x,landmarks[tmp[0]].y],[landmarks[tmp[1]].x,landmarks[tmp[1]].y],[landmarks[tmp[2]].x,landmarks[tmp[2]].y])
-"""
 
-#cap = True
+def get_stages(exceise_dir):
+    return len([name for name in os.listdir(exceise_dir) if os.path.isfile(os.path.join(exceise_dir, name))])
 
-## Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        #frame = cv2.imread("./test/pose.jpg")
-        # Recolor image to RGB
+stage = 0
+#loop = int(input("How many cycles ? :"))
+
+cap = cv2.VideoCapture(camera_index)
+
+#load exceise standard yaml
 
 
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-      
-        # Make detection
-        results = pose.process(image)
+print("there are {} stages".format(get_stages("./exceises/{}".format(exceise_name))))
 
-        if results is None:
-            continue
-    
-        # Recolor back to BGR
+with mp_pose.Pose(
+    min_detection_confidence=0.6,
+    min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    skip = False
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # Flip Image
+    cv2.flip(image,1)
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = pose.process(image)
+
+
+    # judge process
+
+    try:
+        landmarks = results.pose_landmarks.landmark  
+        left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+        left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+        left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+        right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+        right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+        right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+        left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+        right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+        left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+        right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+        elbow_right = calculate_angle(right_shoulder, right_elbow, right_wrist)
+        elbow_left = calculate_angle(left_shoulder, left_elbow, left_wrist)
+        knee_right = calculate_angle(right_hip, right_knee, right_ankle)
+        knee_left = calculate_angle(left_hip, left_knee, left_ankle)
+        hip_left = calculate_angle(left_hip, right_hip, left_knee)
+        points = {"elbow_left":elbow_left,"elbow_right":elbow_right,"knee_left":knee_left,"knee_right":knee_left,"hip_left":hip_left}
         image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
-        # Extract landmarks
-        try:
-            landmarks = results.pose_landmarks.landmark           
-            # Get coordinates
-            left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-            right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-            right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-            left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-            right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
-            right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
-            left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
-            right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+        #armpit_left = calculate_angle(left_hip,left_shoulder,left_elbow)
+        #knee_left_inner = calculate_angle(left_hip,left_knee,left_ankle)
+        #knee_right_inner = calculate_angle(right_hip,right_knee,right_ankle)
+        print("angles:",elbow_right,elbow_left)
+        #if skip is True:
+        #    cv2.imshow('MediaPipe Pose', image)
+        
+        image = cv2.putText(image, "Left elbow:"+str(round(elbow_right,2)), (30,20), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (255,255,255), 1, cv2.LINE_AA)
+        image = cv2.putText(image, "Right elbow:"+str(round(elbow_left,2)), (30,50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (255,255,255), 1, cv2.LINE_AA)
+        image = cv2.putText(image, "Right knee:"+str(round(knee_left,2)), (30,80), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (255,255,255), 1, cv2.LINE_AA)
+        image = cv2.putText(image, "Left knee:"+str(round(knee_right,2)), (30,100), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (255,255,255), 1, cv2.LINE_AA)
+        #judge
 
-            elbow_left = calculate_angle(left_shoulder, left_elbow, left_wrist)
-            armpit_left = calculate_angle(left_hip,left_shoulder,left_elbow)
-            knee_left_inner = calculate_angle(left_hip,left_knee,left_ankle)
-            knee_right_inner = calculate_angle(right_hip,right_knee,right_ankle)
+        f = open(r"./exceises/{}/stage{}.yaml".format(exceise_name,stage),"r")
+        y = yaml.load(f,Loader=yaml.FullLoader)
+        print(y)
+        overall = []
+        for i in range(0,len(y)):
+            
+            key = ""
+            print("hi!")
+            
+            print(y[i])
+            
+            try:
+                print(y[i]["Angle_Name"])
+                if y[i]["Standards"]["Perfect"]["min"] <= points[str(y[i]["Angle_Name"])] < y[i]["Standards"]["Perfect"]["max"]:
+                    print("---------------")
+                    print("Perfect!")
+                    key = "Perfect"
+                    #image = cv2.putText(image, "Perfect!", (30,110), cv2.FONT_HERSHEY_SIMPLEX, 
+                    #1, (255,255,255), 2, cv2.LINE_AA)
+                elif y[i]["Standards"]["Good"]["min"] <= points[str(y[i]["Angle_Name"])] < y[i]["Standards"]["Good"]["max"]:
+                    print("---------------")
+                    print("Good!")
+                    key = "Good"
+                    #image = cv2.putText(image, "Good!", (30,110), cv2.FONT_HERSHEY_SIMPLEX, 
+                    #1, (255,255,255), 2, cv2.LINE_AA)
+                    print("---------------")
+                elif y[i]["Standards"]["Add-oil"]["min"] <= points[str(y[i]["Angle_Name"])] < y[i]["Standards"]["Add-oil"]["max"]:
+                    print("---------------")
+                    print("Add oil!")
+                    key = "Add-oil"
+                    #image = cv2.putText(image, "Add oil!", (30,110), cv2.FONT_HERSHEY_SIMPLEX, 
+                    #1, (255,255,255), 2, cv2.LINE_AA)
+                    print("---------------")
+                else:
+                    key = "Other"
+                overall.append(y[i]["Standards"][key]["score"])
+            except TypeError:
+                image = cv2.putText(image, "Not enough points to score.", (30,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
 
-            points = [elbow_left,armpit_left,knee_left_inner,knee_right_inner]
-
-            if None in points:
-                print("---------------------------")
-                print("Not enough points to score!")
-                print("---------------------------")
-                skip = True
-
-            #print(left_shoulder,left_elbow,left_wrist)
+            
+            if sum(overall) >= (len(y)):
+                stage+=1
+            
+            if stage == get_stages("./exceises/{}".format(exceise_name,stage))-1:
+                stage=0
             
 
-            if not skip:
-                print(elbow_left,armpit_left,knee_left_inner,knee_right_inner)
-                if 150 < elbow_left < 200 and 40 < armpit_left < 80 and 145 < knee_left_inner < 200 and 50 < knee_right_inner < 155:
-                    print("Hello 111")
-                    print("perfect!")
-        except AttributeError:
-            pass
-        except:
-            traceback.print_exc()
-            pass
-        
-        # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
-        
-        if skip and points.count(None)<4:
-            cv2.putText(image, 'No body points', (15,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        elif skip:
-            cv2.putText(image, 'Not enough body points', (15,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        
-        
-        
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+
+                
+    except :
+        image = cv2.putText(image, "No body detected.", (30,30), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (0,0,255), 2, cv2.LINE_AA)
+    print(overall)  
+    print("Stages:{}",stage) 
+    
+    if sum(overall)==len(y) :
+        image = cv2.putText(image, "Perfect".format(str(stage)), (30,170), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+    if sum(overall)>len(y)*2:
+        image = cv2.putText(image, "Perfect".format(str(stage)), (30,170), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)    
+
+    image = cv2.putText(image, "Stage {}".format(str(stage)), (30,170), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+    image = cv2.putText(image, "Overall Score:"+str(sum(overall)), (30,110), cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255), 2, cv2.LINE_AA)
+    
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                 )               
-        
-        cv2.imshow('Feed', image)
-        cv2.moveWindow("Feed",10,10)
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
 
-        if cv2.waitKey(10) in [27,32]:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    #mp_drawing.draw_landmarks(
+    #    image,
+    #    results.pose_landmarks,
+    #    mp_pose.POSE_CONNECTIONS,
+    #    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+    cv2.imshow('MediaPipe Pose', image)
+    key = cv2.waitKey(5)
+    if key in [27]:
+      break
+cap.release()
---
+++ old.py
@@ -0,0 +1,141 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import traceback
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+count=1
+
+cap = cv2.VideoCapture(2)
+
+#cap = cv2.VideoCapture("test/pose.pose.jpg")
+
+skip = False
+
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+
+    if sum(a)+sum(b)+sum(c) > 5:
+        return None
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle 
+"""
+def get_body_angle_by_name(landmarks,angle_name):
+    body_angle_points = {
+                    "armpit_left":[mp_pose.PoseLandmark.LEFT_ELBOW.value,mp_pose.PoseLandmark.LEFT_SHOULDER.value,mp_pose.PoseLandmark.LEFT_WRIST.value],
+                    "armpit_right":[mp_pose.PoseLandmark.RIGHT_ELBOW.value,mp_pose.PoseLandmark.RIGHT_SHOULDER.value,mp_pose.PoseLandmark.RIGHT_WRIST.value],
+                    "elbow_left":[mp_pose.PoseLandmark.LEFT_SHOULDER.value,mp_pose.PoseLandmark.LEFT_ELBOW.value,mp_pose.PoseLandmark.LEFT_WRIST.value],
+                    "elbow_right":[mp_pose.PoseLandmark.RIGHT_SHOULDER.value,mp_pose.PoseLandmark.RIGHT_ELBOW.value,mp_pose.PoseLandmark.RIGHT_WRIST.value],
+                    "knee_left_inner":[mp_pose.PoseLandmark.LEFT_HIP.value,mp_pose.PoseLandmark.LFET_KNEE.value,mp_pose.PoseLandmark.LEFT_ANKLE.value],
+                    "knee_right_inner":[mp_pose.PoseLandmark.RIGHT_HIP.value,mp_pose.PoseLandmark.RIGHT_KNEE.value,mp_pose.PoseLandmark.RIGHT_ANKLE.value]
+                    }
+    if angle_name not in body_angle_points.keys():
+        return None
+    tmp = []
+    tmp = body_angle_points[str(angle_name)]
+    return calculate_angle([landmarks[tmp[0]].x,landmarks[tmp[0]].y],[landmarks[tmp[1]].x,landmarks[tmp[1]].y],[landmarks[tmp[2]].x,landmarks[tmp[2]].y])
+"""
+
+#cap = True
+
+## Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        #frame = cv2.imread("./test/pose.jpg")
+        # Recolor image to RGB
+
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
+
+        if results is None:
+            continue
+    
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark           
+            # Get coordinates
+            left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+            left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+            right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+            left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+            right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+            elbow_left = calculate_angle(left_shoulder, left_elbow, left_wrist)
+            armpit_left = calculate_angle(left_hip,left_shoulder,left_elbow)
+            knee_left_inner = calculate_angle(left_hip,left_knee,left_ankle)
+            knee_right_inner = calculate_angle(right_hip,right_knee,right_ankle)
+
+            points = [elbow_left,armpit_left,knee_left_inner,knee_right_inner]
+
+            if None in points:
+                print("---------------------------")
+                print("Not enough points to score!")
+                print("---------------------------")
+                skip = True
+
+            #print(left_shoulder,left_elbow,left_wrist)
+            
+
+            if not skip:
+                print(elbow_left,armpit_left,knee_left_inner,knee_right_inner)
+                if 150 < elbow_left < 200 and 40 < armpit_left < 80 and 145 < knee_left_inner < 200 and 50 < knee_right_inner < 155:
+                    print("Hello 111")
+                    print("perfect!")
+        except AttributeError:
+            pass
+        except:
+            traceback.print_exc()
+            pass
+        
+        # Render curl counter
+        # Setup status box
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        
+        if skip and points.count(None)<4:
+            cv2.putText(image, 'No body points', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        elif skip:
+            cv2.putText(image, 'Not enough body points', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        
+        
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        
+        cv2.imshow('Feed', image)
+        cv2.moveWindow("Feed",10,10)
+
+        if cv2.waitKey(10) in [27,32]:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
